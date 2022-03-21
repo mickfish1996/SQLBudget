a649a3c4cd510fb5ae02f685e0776a09a890e9ac
mickfish96 <mickfish1996@gmail.com>
2022-03-21 11:55:08 -0600

    finished and added to the read me.

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # SQL Budget Program
+
+My purpose for creating this program was to practice SQL queries and executing them through a python program. I chose a budget because it was a way to enter many items into the database and then filter them by the dates that they were enterd.
+
+[Software Demo Video](https://youtu.be/7MX4-jwLsTM)
+
+# Relational Database
+
+This data base has one table, the transactions table. the items that get put into have an auto incrementing id, the transaction amount and the date that it was enterd into the database. when displaying the items in the database it will only show the items that are from the month that you are currently in.
+
+The transactions table has ID, Amount, and Date. This table has the date in the formate of YYYY-MM-DD
+
+# Development Environment
+
+* Visual Studio Code
+* Python
+* using the sqlite3 environment.
+
+# Useful Websites
+
+* [W3 schools](https://www.w3schools.com/sql/)
+* [stack over flow](https://stackoverflow.com)
+
+# Future Work
+
+* make an option to have other users
+* implement into an app
--- budget.py
+++ budget.py
@@ -4,44 +4,78 @@ from datetime import datetime
 connection = sqlite3.connect('budget.db')
 cursor = connection.cursor()
 
-cursor.execute("CREATE TABLE IF NOT EXISTS users(name TEXT)")
-cursor.execute("CREATE TABLE IF NOT EXISTS transactions(user_id INTEGER, amount FLOAT, date DATETIME)")
+cursor.execute("CREATE TABLE IF NOT EXISTS transactions(ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, amount FLOAT, date_info DATETIME)")
+
 
 
 choice = None
 
-while choice != "6":
+while choice != "5":
     print()
     print("1) Display Users")
-    print("2) Add User")
+    print("2) Add spending")
     print("3) Update Spending")
-    print("4) Reset Spending")
-    print("5) Delete User")
-    print("6) Quit")
+    print("4) Delete Spending")
+    print("5) Quit")
 
     choice = input("> ")
     print()
 
     match choice:
         case "1":
-            cursor.execute("SELECT * FROM users ORDER BY spending")
+            today = datetime.today()
+            current_month = datetime(today.year, today.month, 1)
+
+            value = (current_month,)
+            cursor.execute("SELECT * FROM transactions WHERE date_info >= date('now','start of month')")
 
             
-            print("{:>10}  {:>10}  {:>10}".format("Name", "Spending", "DateTime"))
-
-        
+            print("{:>10}  ${:>10}  {:>10}".format("ID", "AMOUNT", "DATE"))
+            print("-----------------------------------")
+            sum = 0
             for record in cursor.fetchall():
-                print("{:>10}  {:>10}  {:>10}".format(record[0], record[1], record[2]))
+                print("{:>10}  ${:>10}  {:>10}".format(record[0], record[1], record[2]))
+                sum += record[1]
+
+            print("-----------------------------------")
+            print(f"Sum: ${sum}")
 
         case "2":
             try: 
-                name = input("Name: ")
-                values = (name)
+                amount = float(input("Enter Amount: "))
+                date = datetime.today().strftime('%Y-%m-%d')
+                values = (amount, date)
 
-                cursor.execute("INSERT INTO users VALUES (?)", values)
+                cursor.execute("INSERT INTO transactions(amount, date_info) VALUES (?,?)", values)
                 connection.commit()
 
             except ValueError:
-                print("Invalid name: ")
+                print("Invalid Amount: ")
+
+        case "3":
+            try:
+                modify = int(input("Enter the transaction ID that you would like to modify: "))
+                
+                new_amount = float(input("Enter the new amount of the transaction: "))
+                values = (new_amount, modify)
+                cursor.execute("UPDATE transactions SET amount = ? WHERE ID = ?", values)
+                connection.commit()
+
+            except ValueError:
+                print("Invalid ID")
+
+        case "4":
+            try:
+                id = int(input("Enter the transaction ID that you would like to DELETE: "))
+
+                values = (id,)
+
+                cursor.execute("DELETE FROM transactions WHERE ID = ?", values)
+                connection.commit()
+
+            except ValueError:
+                print("Invalid ID")
+
+
 
 connection.close()
